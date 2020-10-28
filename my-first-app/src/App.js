c2739d0b6a05c40fa615b11742f0ba3b1fa911a9
Shelley Ophir <qidcoder@gmail.com>
2020-10-28 17:27:06 -0400

    Adding files

---
+++ my-first-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{fontSize: 60}}>Hello Dojo!</h1>
+      <h2 style={{fontSize: 30}}>Things I need to do:</h2>
+      <ul className="list-items">
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
